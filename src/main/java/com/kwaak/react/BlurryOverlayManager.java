aac5b260748125996b4ebf4200c87a088460adad
Tiago Coelho <tiago.coelho@brandit,pt>
2018-06-25 11:55:47 +0100

    updated ReactProp import

--- src/main/java/com/kwaak/react/BlurryOverlayManager.java
+++ src/main/java/com/kwaak/react/BlurryOverlayManager.java
@@ -1,18 +1,10 @@
 package com.kwaak.react;
 
-import android.app.Activity;
-
-import com.facebook.react.uimanager.ReactProp;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
 public class BlurryOverlayManager extends SimpleViewManager<BlurryOverlayView> {
-    // private Activity activity;
-
-    // public BlurryOverlayManager(Activity activity) {
-    //     this.activity = activity;
-    // }
-
     public static final String REACT_CLASS = "RCTBlurryOverlay";
 
     @Override
